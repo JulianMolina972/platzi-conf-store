0f711d6f3759c67c03544d27850366c05c2beaab
julianmolina99 <juliandavidmv@ufps.edu.co>
2022-07-27 10:23:37 -0500

    Secure and consistent develoment flow with Eslint and prettier+

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from 'react';
+import '../styles/components/app.css';
 
 const App = () => {
-  return (
-    <h1>Hello world</h1>
-  )
-}
+  return <h1>Hello world</h1>;
+};
 
 export default App;
