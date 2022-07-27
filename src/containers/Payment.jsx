ef69383c1e20d0d196adba1e4c3d17d7b11a2b38
julianmolina99 <juliandavidmv@ufps.edu.co>
2022-07-27 15:30:19 -0500

    Payment and success

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import '../styles/components/Payment.css';
 
 export const Payment = () => {
-  return <h1>Payment</h1>;
+  return (
+    <section className="Payment">
+      <div className="Payment-content">
+        <h3>Order Resume</h3>
+        <div className="Payment-button">
+          buy button with Paypal
+        </div>
+      </div>
+      <aside></aside>
+
+    </section>
+  );
 };
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import '../styles/components/Success.css';
 
 export const Success = () => {
-  return <h1>Success</h1>;
+  return (
+    <section className="Success">
+      <div className="Success-content">
+        <h2>Julian, Thank you for your purchase</h2>
+        <span>Your order arrive in three days to your address</span>
+        <div className='Success-map'>
+          Google Maps
+        </div>
+      </div>
+    </section>
+
+  );
 };
